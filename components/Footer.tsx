558e4f51859d9c17c6ffc309949dc7f60c924a79
Your Name <your-email@example.com>
2026-04-30 19:32:25 +0530

    chore: add startup benchmarks badge to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -194,6 +194,12 @@ export default function Footer() {
             Featured on SeeWhatNewAI Directory
           </a>
         </div>
+        <div className="mb-8 mt-4 flex justify-center">
+          <a target="_blank" href="https://startupbenchmarks.com" rel="noreferrer">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="https://startupbenchmarks.com/assets/images/badge.png" alt="Startup Benchmarks" height="54" />
+          </a>
+        </div>
       </div>
     </footer>
   )
